b9ac5338710fcf7868cda2f01c9fa921cdc41504
OneNakedBass <71959291+OneNakedBass@users.noreply.github.com>
2021-05-07 13:11:07 -0700

    Fixed regex for phone validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ApplicationRecord
 
     validates :first_name, format: { :multiline => true, with: /^[a-z]+$/i, message: "can only contain letters" }
     validates :last_name, format: { :multiline => true, with: /^[a-z]+$/i, message: "can only contain letters" }
-    validates :phone, format: { :multiline => true, with: /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/ }
+    validates :phone, format: { :multiline => true, with: /(^$)|(^\d{10}$)/ }
     validates :company, format: { :multiline => true, with: /^[a-zA-Z0-9]*$/, message: "can only contain letters and numbers with no spaces"}
     validates :phone, uniqueness: true
     validates :email, format: { :multiline => true, with: /^(.+)@(.+)$/ }
